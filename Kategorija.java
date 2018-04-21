5cc9d10e9c04f0926ce6b0fae41249871ca466ee
Cvijetin Vidakovic <vidakovic.cvijetin@gmail.com>
2018-04-21 07:07:18 +0200

    Kategorija

---
+++ Kategorija.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+/**
+ *
+ * @author Cvijetin
+ */
+public class Kategorija {
+    private String naziv;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+    
+}
